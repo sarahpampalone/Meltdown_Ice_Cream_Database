499b01a686630be79e7a315da2268dc3824d82c3
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2023-11-29 16:36:34 -0500

    Update method_inventory.py

--- method_inventory.py
+++ method_inventory.py
@@ -20,13 +20,14 @@ def take_inventory(ice_cream_inventory, topping_inventory, paper_inventory):
     monday = input().strip()
 
     full_sales = []
-    current = []
+    fullinventory = []
 
     print("Okay! Let's start with ice cream flavors")
     for flavor_list in ice_cream_inventory:
         for flavor in flavor_list:
             print(f"{flavor.name}: ")
             count = int(input())
+            fullinventory.append(flavor.name)
             full_sales.append(flavor.standard_amount - count)
 
     print("\nNow toppings")
@@ -34,6 +35,7 @@ def take_inventory(ice_cream_inventory, topping_inventory, paper_inventory):
         for topping in topping_list:
             print(f"{topping.name}: ")
             count = int(input())
+            fullinventory.append(topping.name)
             full_sales.append(topping.standard_amount - count)
 
     print("\nNow paper products")
@@ -41,18 +43,8 @@ def take_inventory(ice_cream_inventory, topping_inventory, paper_inventory):
         for paper in paper_list:
             print(f"{paper.name}: ")
             count = int(input())
-            full_sales.append(paper.standard_amount - count)
-
-    fullinventory = []
-    for flavor_list in ice_cream_inventory:
-        for flavor in flavor_list:
-            fullinventory.append(flavor.name)
-    for topping_list in topping_inventory:
-        for topping in topping_list:
-            fullinventory.append(topping.name)
-    for product_list in paper_inventory:
-        for product in product_list:
             fullinventory.append(product.name)
+            full_sales.append(paper.standard_amount - count)
 
     print("\nAre you\n1. Placing an order today\n2. Wanting an overall summary")
     choice = input()
